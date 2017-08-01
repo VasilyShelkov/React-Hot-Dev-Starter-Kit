15ac0d11ca43643d982d402df8481471eb00d32d
Vasily Shelkov <vasilydshelkov@gmail.com>
2017-08-01 22:22:21 +0100

    added renderer to the globals

--- testSetup.js
+++ testSetup.js
@@ -1,4 +1,5 @@
 require("jest-styled-components");
+const renderer = require("react-test-renderer");
 const sinon = require("sinon");
 const sinonTest = require("sinon-test");
 const shallow = require("enzyme").shallow;
@@ -7,3 +8,4 @@ sinon.test = sinonTest.configureTest(sinon);
 
 global.sinon = sinon;
 global.shallow = shallow;
+global.renderer = renderer;
